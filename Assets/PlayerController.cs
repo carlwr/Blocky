6f2542008a9355d77d89cdfa8560c22ef1679ea6
carl <carlwrosen@gmail.com>
2019-03-06 19:28:52 +0000

    fix problem with new boxes could be set on plattforms

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour {
 
 	public float jumpForce;
 	public Tilemap tm;
+
 	public TileBase tb;
 	public Tilemap nextTiles;
 	public float speed;
@@ -54,19 +55,37 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void showTilesToChoose(){
-		if(tm.GetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y + 1,0)) == null){
+		
+		nextTiles.SetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y + 1, 0), null);
+		nextTiles.SetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y - 1, 0), null);
+		nextTiles.SetTile(new Vector3Int(lastAddedTile.x+1, lastAddedTile.y, 0), null);
+		nextTiles.SetTile(new Vector3Int(lastAddedTile.x-1, lastAddedTile.y , 0), null);
+
+		Vector3 tilemapWorld = tm.CellToWorld(lastAddedTile);
+		tilemapWorld.x += 0.5f;
+		tilemapWorld.y += 0.5f;
+
+		RaycastHit2D hit = Physics2D.Raycast(new Vector2(tilemapWorld.x, tilemapWorld.y + 1),
+											 Vector2.zero);
+		if(hit.collider == null){
 			nextTiles.SetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y + 1, 0), tb);
 		}
-		
-		if(tm.GetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y - 1,0)) == null){
+
+		hit = Physics2D.Raycast(new Vector2(tilemapWorld.x, tilemapWorld.y - 1),
+											 Vector2.down, 0.1f);
+		if(hit.collider == null){
 			nextTiles.SetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y - 1, 0), tb);
 		}
-		
-		if(tm.GetTile(new Vector3Int(lastAddedTile.x +1, lastAddedTile.y,0)) == null){
+
+		hit = Physics2D.Raycast(new Vector2(tilemapWorld.x+1, tilemapWorld.y),
+											 Vector2.right, 0.1f);
+		if(hit.collider == null){
 			nextTiles.SetTile(new Vector3Int(lastAddedTile.x + 1, lastAddedTile.y, 0), tb);
 		}
-		
-		if(tm.GetTile(new Vector3Int(lastAddedTile.x -1, lastAddedTile.y,0)) == null){
+
+		hit = Physics2D.Raycast(new Vector2(tilemapWorld.x-1, tilemapWorld.y),
+											 Vector2.left, 0.1f);
+		if(hit.collider== null){
 			nextTiles.SetTile(new Vector3Int(lastAddedTile.x - 1, lastAddedTile.y, 0), tb);
 		}
 	}
@@ -110,8 +129,15 @@ public class PlayerController : MonoBehaviour {
 			chooseNextBox = true;
 		}
 		if(chooseNextBox){
+			
+			Vector3 tilemapWorld = tm.CellToWorld(lastAddedTile);
+			tilemapWorld.x += 0.5f;
+			tilemapWorld.y += 0.5f;
+
+			RaycastHit2D hit = Physics2D.Raycast(new Vector2(tilemapWorld.x, tilemapWorld.y + 1),
+											 Vector2.zero);
 			if (Input.GetKey("w") && 
-			tm.GetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y + 1,0)) == null)
+			hit.collider == null)
 			{
 				removeTilesToShow();
 				lastAddedTile += new Vector3Int(0,1,0);
@@ -119,7 +145,8 @@ public class PlayerController : MonoBehaviour {
 				state = State.NORMAL;
 			}
 			else if (Input.GetKey("d") && 
-			tm.GetTile(new Vector3Int(lastAddedTile.x + 1, lastAddedTile.y ,0)) == null)
+			Physics2D.Raycast(new Vector2(tilemapWorld.x+1, tilemapWorld.y),
+											 Vector2.right, 0.1f).collider	 == null)
 			{	
 				removeTilesToShow();
 				lastAddedTile += new Vector3Int(1,0,0);
@@ -127,7 +154,8 @@ public class PlayerController : MonoBehaviour {
 				state = State.NORMAL;
 			}
 			else if (Input.GetKey("a") && 
-			tm.GetTile(new Vector3Int(lastAddedTile.x -1, lastAddedTile.y,0)) == null)
+			Physics2D.Raycast(new Vector2(tilemapWorld.x-1, tilemapWorld.y),
+											 Vector2.left, 0.1f).collider == null)
 			{	
 				removeTilesToShow();
 				lastAddedTile += new Vector3Int(-1,0,0);
@@ -136,7 +164,8 @@ public class PlayerController : MonoBehaviour {
 				state = State.NORMAL;
 			}
 			else if (Input.GetKey("s") && 
-			tm.GetTile(new Vector3Int(lastAddedTile.x, lastAddedTile.y-1,0)) == null)
+			Physics2D.Raycast(new Vector2(tilemapWorld.x, tilemapWorld.y - 1),
+											 Vector2.down, 0.1f).collider == null)
 			{	
 				removeTilesToShow();
 				lastAddedTile += new Vector3Int(0,-1,0);
