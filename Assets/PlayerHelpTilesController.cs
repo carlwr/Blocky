95fecffae461d7b4ff889ea291b860e7b5398aba
carl <carlwrosen@gmail.com>
2019-03-15 15:42:57 +0000

    boxes grey when inventory empty

--- Assets/PlayerHelpTilesController.cs
+++ Assets/PlayerHelpTilesController.cs
@@ -16,6 +16,7 @@ public class PlayerHelpTilesController : TilemapController
    }
 
     void Update(){
+        
        if(PlayerController.instance.state == PlayerController.State.ADD_TILE){
            showTilesToChoose();
             updateSelectedTileColor();
@@ -51,21 +52,35 @@ public class PlayerHelpTilesController : TilemapController
 
     void updateSelectedTileColor(){
         
-       float wButtonDown = PlayerController.instance.getWButton();
-       float sButtonDown = PlayerController.instance.getSButton();
-       float aButtonDown = PlayerController.instance.getAButton();
-       float dButtonDown = PlayerController.instance.getDButton();
-       
         Vector3Int lastAddedTile = PlayerController.instance.getLastAddedTile();
+        if(PlayerController.instance.boxesInInventory < 1){
+            
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x, lastAddedTile.y + 1, 0), new Color(0.5f,0.5f, 0.5f, 0.3f));
+            
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x, lastAddedTile.y - 1, 0), new Color(0.5f,0.5f, 0.5f, 0.3f));
         
-        tilemap.SetColor(new Vector3Int(lastAddedTile.x, lastAddedTile.y + 1, 0), new Color(wButtonDown,1-wButtonDown, 1-wButtonDown));
-		
-        tilemap.SetColor(new Vector3Int(lastAddedTile.x, lastAddedTile.y - 1, 0), new Color(sButtonDown,1-sButtonDown, 1-sButtonDown));
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x + 1, lastAddedTile.y, 0), new Color(0.5f,0.5f, 0.5f, 0.3f));
+        
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x - 1, lastAddedTile.y, 0), new Color(0.5f,0.5f, 0.5f, 0.3f));
+        
+            }
+        else{
+            float wButtonDown = PlayerController.instance.getWButton();
+            float sButtonDown = PlayerController.instance.getSButton();
+            float aButtonDown = PlayerController.instance.getAButton();
+            float dButtonDown = PlayerController.instance.getDButton();
+            
+                
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x, lastAddedTile.y + 1, 0), new Color(wButtonDown,1-wButtonDown, 1-wButtonDown));
+            
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x, lastAddedTile.y - 1, 0), new Color(sButtonDown,1-sButtonDown, 1-sButtonDown));
+        
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x + 1, lastAddedTile.y, 0), new Color(dButtonDown,1-dButtonDown, 1-dButtonDown));
+        
+            tilemap.SetColor(new Vector3Int(lastAddedTile.x - 1, lastAddedTile.y, 0), new Color(aButtonDown,1-aButtonDown, 1-aButtonDown));
+        
+            }
+        }
     
-        tilemap.SetColor(new Vector3Int(lastAddedTile.x + 1, lastAddedTile.y, 0), new Color(dButtonDown,1-dButtonDown, 1-dButtonDown));
-    
-        tilemap.SetColor(new Vector3Int(lastAddedTile.x - 1, lastAddedTile.y, 0), new Color(aButtonDown,1-aButtonDown, 1-aButtonDown));
-    
-    }
 
 }
