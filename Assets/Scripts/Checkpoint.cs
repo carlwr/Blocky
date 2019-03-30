0b4f0c72f89ecf4656deac586bd642409fbbce36
carl <carlwrosen@gmail.com>
2019-03-30 17:08:11 +0000

    rocket ship!

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -17,19 +17,18 @@ public class Checkpoint : MonoBehaviour
                                                 +  PlayerController.instance.getPlayerTiles().Count-1;
             
             BoundsInt bounds = GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().cellBounds;
-        TileBase[] allTiles = GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().GetTilesBlock(GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().cellBounds);
+            TileBase[] allTiles = GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().GetTilesBlock(GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().cellBounds);
 
-        for (int x = 0; x < bounds.size.x; x++) {
-            for (int y = 0; y < bounds.size.y; y++) {
-                TileBase tile = allTiles[x + y * bounds.size.x];
-                if (tile != null) {
-                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
-                } else {
-                   // Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+            for (int x = 0; x < bounds.size.x; x++) {
+                for (int y = 0; y < bounds.size.y; y++) {
+                    TileBase tile = allTiles[x + y * bounds.size.x];
+                    if (tile != null) {
+                        Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                    } else {
+                    // Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                    }
                 }
-            }
-        }  
-            //Debug.Log(GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().GetTilesBlock(GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().cellBounds));
+            }  
             GameMaster.instance.pickups = GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().GetTilesBlock(GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().cellBounds);
 
             GameMaster.instance.pickupsBounds = GameObject.FindGameObjectWithTag("Pickups").GetComponent<Tilemap>().cellBounds;
