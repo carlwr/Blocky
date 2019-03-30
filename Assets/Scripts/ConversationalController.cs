481999d7c09d0f08de33d6f3f05597da0a04f0b2
Dagmar <margretl16@ru.is>
2019-03-30 14:24:31 +0000

    Gameobject prefab for conversational agent, platform ruletile fixed slightly

--- Assets/Scripts/ConversationalController.cs
+++ Assets/Scripts/ConversationalController.cs
@@ -5,15 +5,16 @@ using UnityEngine.Tilemaps;
 using Hellmade.Sound;
 public class ConversationalController : MonoBehaviour
 {
+    public string text;
     public AudioClip[] gibberishNpcTalking;
     
     static public int[] gibberish;
     public System.Random rand = new System.Random();
-    Tilemap tilemap;
+    
     // Start is called before the first frame update
     void Start()
     {
-        tilemap = gameObject.GetComponent<Tilemap>();
+
     }
 
     // Update is called once per frame
@@ -32,25 +33,13 @@ public class ConversationalController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.tag == "Player"){
-            
             // Generate a random index less than the size of the array.  
             int index = rand.Next(gibberishNpcTalking.Length);
             //Jump sound played
-            Debug.Log("index:" + index);
-            Debug.Log("array length     :" + gibberishNpcTalking.Length);
-
             int gibberishID = EazySoundManager.PlaySound(gibberishNpcTalking[index], 0.5f);
-            for (int i = (int)collider.transform.position.x - 3; i < (int)collider.transform.position.x + 3; i++){
-                for(int j = (int)collider.transform.position.y - 3; j < (int)collider.transform.position.y + 3; j++){
-                    var pPos = tilemap.WorldToCell(new Vector2(i,j));
-                    if(tilemap.GetTile(pPos) != null){
-                        string text = ((ConversationalAgentTile)tilemap.GetTile(new Vector3Int(i,j,0))).text;
-                        UIController.instance.conversation.text = text;
-                        UIController.instance.setConversationPosition(new Vector3(i, j + 4, 0));
-                    }
-                }   
-            }
-
+            var pPos = transform.position;
+            UIController.instance.conversation.text = text;
+            UIController.instance.setConversationPosition(new Vector3(transform.position.x, transform.position.y + 4, 0));
         }
     }
 
