e8068183624e2cccc199c38042eff6efbc9e2bd5
carl <carlwrosen@gmail.com>
2019-03-30 16:01:30 +0000

    new button

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class buttonScript : MonoBehaviour
+{
+    public GameObject tilemapObject;
+    Tilemap tilemap;
+    public Sprite buttonPressed;
+    public Sprite buttonNotPressed;
+    public TileBase bridgeOpen;
+    public TileBase bridgeClosed;
+    public int cooldown;
+    TilemapCollider2D tileMapCollider;
+    
+    void Start()
+    {
+        tilemap = tilemapObject.GetComponent<Tilemap>();
+        tileMapCollider = tilemapObject.GetComponent<TilemapCollider2D>();
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {   
+            GetComponent<SpriteRenderer>().sprite = buttonPressed;
+
+            tilemapObject.tag = "Plattform";
+            tileMapCollider.isTrigger = false;
+            BoundsInt bounds = tilemap.cellBounds;
+            List<Vector3Int> bridgeTiles = new List<Vector3Int>();
+            for(int x = bounds.xMin; x < bounds.xMax; x++){
+                for(int y = bounds.yMin; y < bounds.yMax; y++){
+                    if(tilemap.GetTile(new Vector3Int(x,y,0)) != null){
+                        bridgeTiles.Add(new Vector3Int(x,y,0));
+                        tilemap.SetTile(new Vector3Int(x,y,0), bridgeOpen);
+                    }
+                }   
+            }
+            StartCoroutine(ButtonCooldown(bridgeTiles));
+                
+        }
+        
+    }
+
+    IEnumerator ButtonCooldown(List<Vector3Int> bridgeTiles)
+    {
+        yield return new WaitForSeconds(cooldown);
+        
+        GetComponent<SpriteRenderer>().sprite = buttonNotPressed;
+        for (int i = 0; i < bridgeTiles.Count; i++)
+        {
+               tilemap.SetTile(bridgeTiles[i], bridgeClosed);
+        }
+        tileMapCollider.isTrigger = true;
+        
+        tilemapObject.tag = "Untagged";
+    }
+}
